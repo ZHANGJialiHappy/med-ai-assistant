852b2290a25d9ead88b64c43f6dfba77c36c6e84
Jiali <jiali.zhang@man-es.com>
2025-07-21 12:52:28 +0200

    Navbar finished

---
+++ frontend/src/components/Navbar.tsx
@@ -0,0 +1,12 @@
+import Link from 'next/link'
+
+export default function Navbar() {
+  return (
+    <nav className="bg-blue-600 text-white px-4 py-2 flex gap-4">
+      <Link href="/">Home</Link>
+      <Link href="/ask">Ask Question</Link>
+      <Link href="/analyze">Analyze Report</Link>
+      <Link href="/history">History</Link>
+    </nav>
+  )
+}
